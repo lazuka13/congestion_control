dbd5cbcf509948a095589ae286c3b515ff586b95
lazuka13 <lazuka13@gmail.com>
2020-05-29 12:29:25 +0300

    Changes

--- main.py
+++ main.py
@@ -18,14 +18,16 @@ ROOT_DIR = "/home/simuser/repos/dce-4.19.79/source/ns-3-dce/"
 
 
 @click.command()
-@click.option('--congestion_control', '-c', required=True, help='congestion control algorithm',
+@click.option('--congestion_control', '-c', required=False, help='congestion control algorithm',
               type=click.Choice(CONGESTION_CONTROLS_ALGORITHMS), multiple=True)
 def run(congestion_control):
+    if not congestion_control:
+        congestion_control = CONGESTION_CONTROLS_ALGORITHMS
     results = dict()
     for cc in congestion_control:
         results[cc] = list()
-        for error_rate in [5, 10, 15, 20, 25, 50]:
-            error_rate_percent = error_rate / 100.0
+        for error_rate in [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]:
+            error_rate_percent = error_rate / 10000.0
             results_folder = "{0}_{1}".format(cc, error_rate)
             results_folder_full = os.path.join(ROOT_DIR, "results", results_folder)
             results_folder_local = os.path.join(".", "data", results_folder)
@@ -37,32 +39,35 @@ def run(congestion_control):
                 error_rate_percent=error_rate_percent,
                 results_folder=results_folder)
             if not os.path.exists(results_folder_local):
-                continue
-                # print("starting: {0}".format(args))
-                # subprocess.check_call(args, shell=True, cwd=ROOT_DIR)
-                # shutil.copytree(results_folder_full, results_folder_local)
+                print("starting: {0}".format(args))
+                subprocess.check_call(args, shell=True, cwd=ROOT_DIR)
+                shutil.copytree(results_folder_full, results_folder_local)
             else:
                 print("skipping: {0}".format(args))
 
             with open(os.path.join(results_folder_local, "throughput", "client.log")) as server_log_file:
                 server_log = [l.strip() for l in server_log_file.readlines() if l.strip()]
 
-            b_total, t = 0, 0
+            b_total, t_start, t_end = 0, None, 0
             for line in server_log:
                 t, b = line.split(" ")
-                b_total += float(b) * 8
-            results[cc].append([error_rate, b_total / (float(t) * 1024 ** 3)])
+                if t_start is None:
+                    t_start = float(t)
+                t_end = float(t)
+                b_total += float(b) * 8 * 10.0 / 1000  # convert to bytes and * 10 ms == sent in this 10 ms
+            results[cc].append(b_total / (float(t_end - t_start) * 1024 ** 3))  # utilization
 
-        x, y = [], []
-        for (error_rate, throughput) in results[cc]:
-            x.append(error_rate)
-            y.append(throughput)
+    error_rate_percents = [x / 10000.0 for x in [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]]
 
-        plt.clf()
-        plt.plot(x, y)
-        plt.xlabel('error_rate')
-        plt.ylabel('throughput percentage')
-        plt.savefig('./graphs/{0}_result.png'.format(cc))
+    for cc in congestion_control:
+        plt.plot(error_rate_percents, results[cc], label=cc)
+
+    plt.xlabel('error_rate_percentage')
+    plt.ylabel('throughput percentage')
+    plt.margins(0.05)
+    legend = plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
+                        ncol=3, fancybox=True, shadow=True)
+    plt.savefig('./result.png', bbox_extra_artists=(legend,), bbox_inches='tight')
 
 
 if __name__ == "__main__":
